0a6725938788f4d82b7e061e671f235d784fe6c7
Quant Aurelius <77588340+daniellopez-2@users.noreply.github.com>
2024-05-07 22:25:53 -0600

    Update hero.tsx

--- web/src/components/furniture/hero.tsx
+++ web/src/components/furniture/hero.tsx
@@ -7,14 +7,13 @@ export default component$(() => {
     <div class="hero mb-8 mx-auto xl:max-w-7xl max-w-6xl w-full xl:px-10">
       <div class="hero-content text-center bg-front shadow-sm lg:rounded-xl w-full">
         <div class="max-w-2xl flex flex-col place-items-center">
-          <p>The Ultimate</p>
           <h1 class="text-5xl font-bold">Personal Security Checklist</h1>
           <p class="subtitle pb-6">Your guide to securing your digital life and protecting your data privacy</p>
           <Icon class="mb-4" icon="shield" width={120} height={120}  />
-          <a href="https://indexbug.com">
+          <a href="https://www.aiappsblocks.dev/">
             <button class="btn btn-primary btn-lg">
               <Icon icon="github" width={20} height={20}  />
-              Go to IndexBug
+              Create your Startup
             </button>
           </a>
         </div>
